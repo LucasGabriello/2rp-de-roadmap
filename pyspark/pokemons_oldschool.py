f2cbca48107656a9b3e2eaecdeee1e06e10ba3b3
lucasgabriello <lucasgda59@gmail.com>
2022-08-25 18:06:38 -0300

    add pyspark script

---
+++ pyspark/pokemons_oldschool.py
@@ -0,0 +1,36 @@
+%pyspark
+from pyspark.sql.dataframe import DataFrame
+import pyspark.sql.functions
+
+
+
+
+#3.1. carregue os dados das tabelas pokemon e generation em dois dataframes distintos
+
+df_pokemon = spark.table("work_dataeng.pokemon_lucas")
+df_generation = spark.table("work_dataeng.generation_lucas")
+df_pok_gen = spark.table("work_dataeng.pokemons_oldschool_lucasa")
+
+
+
+
+#3.2. filtre o dataframe com os dados de generation para pegar somente as gerações inferiores a 2002-11-21, e em seguida faça um cache deste dataframe;
+
+df_generation = df_generation.filter("date_introduced < '2002-11-21'");
+df_generation = df_generation.cache()
+df_generation.show()
+
+
+
+
+#3.3. em um novo dataframe atribua o inner join entre generation e pokemon;
+
+df_pok_gen = df_pokemon.join(df_generation, 'generation', how = 'inner')
+df_pok_gen.show()
+
+
+
+
+#3.4. por fim, salve o novo dataframe em uma tabela chamada work_dataeng.pokemons_oldschool_lucasa.
+
+df_pok_gen.write.saveAsTable('work_dataeng.pokemons_oldschool_lucasa')
